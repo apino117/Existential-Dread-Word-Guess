ef5604fc8512013d994c61ac9270b2fd4b4cb445
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-02-25 15:50:12 -0500

    got blanks matching again, but better because blankguess is established

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -15,12 +15,12 @@ var answerWord = [
 // Establish blank variables as individual spaces
 var freeBlank = ["_", "_", "_", "_"];
 var scaredBlank = ["_", "_", "_", "_", "_"];
-var controlBlank = ["_", "_", "_", "_", "_", "_"];
-var paranoiaBlank = ["_", "_", "_", "_", "_", "_", "_"];
+var controlBlank = ["_", "_", "_", "_", "_", "_", "_"];
+var paranoiaBlank = ["_", "_", "_", "_", "_", "_", "_", "_"];
 
 
 // Array of blanks
-var blankGuess = [
+var blankWords = [
     freeBlank,
     scaredBlank,
     controlBlank,
@@ -37,17 +37,45 @@ var computerGuess = answerWord[Math.floor(Math.random() * answerWord.length)];
 
 // Make blank guess line up with computer guess
 
-// For every index of the answer word array
-for (i = 0; i<answerWord.length; i++) {
+// So for each word in the answer array
+for (i = 0; i < answerWord.length; i++) {
 
-    // If the computer guess is index[i]
+    // If the computer guesses word [i]
     if (computerGuess == answerWord[i]) {
-        
-        // Change the text content of blank
-        blankslotText.textContent = blankGuess[i];
+
+        // Blank guess will be blankword[i]
+        var blankGuess = blankWords[i];
     }
 }
 
-console.log(computerGuess);
 console.log(blankGuess);
+console.log(computerGuess);
+blankslotText.textContent = blankGuess
 
+
+
+// // Capture user guess (spoiler alert, everything happens in this function)
+
+// // When a key is hit
+// document.onkeyup = function () {
+
+//     // Making sure all is lower case
+//     userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+//     // Shows us what we typed
+//     console.log(userGuess);
+
+//     // Determine if the key was correct or not 
+
+//     // 1. Loop through each letter in the guess
+//     for (i = 0; i < computerGuess.length; i++) {
+        
+//         // If the userguess is a key within the computer guess
+//         if (userGuess == computerGuess[i]) {
+            
+//             // Replace the blank slot accordingly
+//             blankGuess.splice(i, 1, userGuess);
+
+//         }
+//     }
+// }
