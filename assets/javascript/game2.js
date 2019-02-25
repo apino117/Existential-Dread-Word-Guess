6284d935ddf2143d9be94472a2d857484363e7ca
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-02-25 15:15:00 -0500

    got blank slots lining up

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,12 +1,10 @@
-// Establish Answer Words
-
-// Variables as individual letters
+// Establish answer words as individual letters
 var free = ["f", "r", "e", "e"];
 var scared = ["s", "c", "a", "r", "e", "d"]
 var control = ["c", "o", "n", "t", "r", "o", "l"]
 var paranoia = ["p", "a", "r", "a", "n", "o", "i", "a"]
 
-// Array of variabled-out answer words
+// Array of answers
 var answerWord = [
     free,
     scared,
@@ -29,8 +27,27 @@ var blankGuess = [
     paranoiaBlank,
 ]
 
-console.log(blankGuess)
+// Establish the blankslotText variable in accordance to HTML
+var blankslotText = document.getElementById("blankslot-text");
+
 
 // Computer needs to make a selection
 var computerGuess = answerWord[Math.floor(Math.random() * answerWord.length)];
+
+
+// Make blank guess line up with computer guess
+
+// For every index of the answer word array
+for (i = 0; i<answerWord.length; i++) {
+
+    // If the computer guess is index[i]
+    if (computerGuess == answerWord[i]) {
+        
+        // Change the text content of blank
+        blankslotText.textContent = blankGuess[i];
+    }
+}
+
 console.log(computerGuess);
+console.log(blankGuess);
+
