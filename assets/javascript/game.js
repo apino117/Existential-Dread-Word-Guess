dd59e9b97450def0340c84c13573bd6c1c16d917
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-02-26 22:20:38 -0500

    doesnt reload with wins but otherwise we're good

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -46,12 +46,17 @@ var blankWords = [
 // Establish the blankslotText variable in accordance to HTML
 var blankslotText = document.getElementById("blankslot-text");
 
-// Establish a guessesremaining counter
-var guessremainCounter = 15;
+// Establish a guessesremaining & a win counter
+var guessremainCounter = 13;
+var winCounter = 0;
 
 // Link guessremain counter to HTML
 var guessremainText = document.getElementById("guessesremaining-text")
 guessremainText.textContent = ("Guesses Remaining: " + guessremainCounter);
+
+// Link win counter to HTML
+var winsText = document.getElementById("wins-text")
+winsText.textContent = ("Wins: " + winCounter);
 
 // Establish letters guessed array
 var lettersguessedArray = []
@@ -63,88 +68,98 @@ lettersGuessed.textContent = ("Letters Guessed: " + lettersguessedArray)
 // Computer needs to make a selection
 var computerGuess = answerWord[Math.floor(Math.random() * answerWord.length)];
 
+function chooseWord() {
+    // Make blank guess line up with computer guess
 
+    // So for each word in the answer array
+    for (i = 0; i < answerWord.length; i++) {
 
-// Make blank guess line up with computer guess
+        // If the computer guesses word [i]
+        if (computerGuess == answerWord[i]) {
 
-// So for each word in the answer array
-for (i = 0; i < answerWord.length; i++) {
-
-    // If the computer guesses word [i]
-    if (computerGuess == answerWord[i]) {
-
-        // Blank guess will be blankword[i]
-        var blankGuess = blankWords[i];
-    }
-}
-console.log(blankGuess);
-console.log(computerGuess);
-
-// Update HTML of blankguess 
-blankslotText.textContent = blankGuess
-
-
-
-// Capture user guess (spoiler alert, everything happens in this function)
-
-// When a key is hit
-document.onkeyup = function () {
-
-    var isGuessed = false;
-
-    // Making sure all is lower case
-    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
-    // Shows us what we typed
-    console.log(userGuess);
-
-    // Push letter to already guessed array
-    lettersguessedArray.push(userGuess);
-
-    // Update HTML
-    lettersGuessed.textContent = ("Letters Guessed: " + lettersguessedArray)
-
-    // Determine if the key was correct or not 
-
-    // 1. Loop through each letter in the guess
-    for (i = 0; i < computerGuess.length; i++) {
-
-        // 2. If the userguess is a key within the computer guess
-        if (userGuess == computerGuess[i]) {
-
-            // 3. Replace the blank slot accordingly
-            blankGuess.splice(i, 1, userGuess);
-
-            // 4. Update HTML
-            blankslotText.textContent = blankGuess;
-
-            isGuessed = true;
-
-        } 
-    }
-    if (!isGuessed) {
-
-        // Lose a guess
-        guessremainCounter--;
-
-        //Update HTML
-        guessremainText.textContent = ("Guesses Remaining: " + guessremainCounter);
-
-        // If no guesses left
-        if (guessremainCounter === 0) {
-
-            //Lose screen
-            alert("Ah...you've lost...that's probably okay...");
-
-            // Reset everything
-            location.reload();
+            // Blank guess will be blankword[i]
+            var blankGuess = blankWords[i];
         }
     }
-    // If the blankguess doesnt have anymore "_" in it
-    if (!blankGuess.includes("_")) {
-        
-        // Win condition
-        alert("win");
+    console.log(blankGuess);
+    console.log(computerGuess);
+
+    // Update HTML of blankguess 
+    blankslotText.textContent = blankGuess
+    // Capture user guess (spoiler alert, everything happens in this function)
+
+    // When a key is hit
+    document.onkeyup = function () {
+
+        var isGuessed = false;
+
+        // Making sure all is lower case
+        userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+        // Shows us what we typed
+        console.log(userGuess);
+
+        // Push letter to already guessed array
+        lettersguessedArray.push(userGuess);
+
+        // Update HTML
+        lettersGuessed.textContent = ("Letters Guessed: " + lettersguessedArray)
+
+        // Determine if the key was correct or not 
+
+        // 1. Loop through each letter in the guess
+        for (i = 0; i < computerGuess.length; i++) {
+
+            // 2. If the userguess is a key within the computer guess
+            if (userGuess == computerGuess[i]) {
+
+                // 3. Replace the blank slot accordingly
+                blankGuess.splice(i, 1, userGuess);
+
+                // 4. Update HTML
+                blankslotText.textContent = blankGuess;
+
+                isGuessed = true;
+
+            }
+        }
+        if (!isGuessed) {
+
+            // Lose a guess
+            guessremainCounter--;
+
+            //Update HTML
+            guessremainText.textContent = ("Guesses Remaining: " + guessremainCounter);
+
+            // If no guesses left
+            if (guessremainCounter === 0) {
+
+                //Lose screen
+                alert("Ah...you've lost...that's probably okay...");
+
+                // Reset everything
+                location.reload();
+            }
+        }
+        // If the blankguess doesnt have anymore "_" in it
+        if (!blankGuess.includes("_")) {
+
+            // Win condition
+            alert("Ah...you've won...I'm free!");
+
+            // Add a win counter
+            winCounter++;
+
+            winsText.textContent = ("Wins: " + winCounter);
+
+            // Reset
+            // Didn't figure out how to reset without reloading whole page
+
+        }
     }
 }
+
+chooseWord();
+
+
 
